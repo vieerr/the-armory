5d925e72aff2a14810afb5481c6909290cf752bb
vieerr <olivier_paspuel@hotmail.com>
2025-08-12 19:46:31 -0500

    feat: qol changes and sync between components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ProductDetails } from './products/components/product-details/product-details';
 import { ProductLayout } from './products/components/product-layout/product-layout';
-
+import { ShoppingCart } from './shopping-cart/components/shopping-cart/shopping-cart';
+import { Checkout } from './billing/components/order-details/checkout';
+import { OrderConfirmationComponent } from './billing/components/confirmation/confirmation';
+import { OrdersLayot } from './orders/components/orders-layot/orders-layot';
 export const routes: Routes = [
-
+  {path: 'orders', component: OrdersLayot},
+  { path: 'cart', component: ShoppingCart },
+  { path: 'checkout/confirmation', component: OrderConfirmationComponent },
+  { path: 'checkout', component: Checkout },
   { path: 'products/:id', component: ProductDetails },
   { path: '', component: ProductLayout },
-
 ];
--- src/app/components/general/navbar/navbar.ts
+++ src/app/components/general/navbar/navbar.ts
@@ -1,15 +1,59 @@
 import { Component } from '@angular/core';
-import { cartService } from '../../../shopping-cart/cart-service';
+import { cartService } from '../../../shopping-cart/services/cart-service';
+import { RouterLink } from '@angular/router';
+import {
+  gameArchiveResearch,
+  gameBackpack,
+  gameBlackKnightHelm,
+  gameBookCover,
+  gameBookPile,
+  gameBoxUnpacking,
+  gameCrown,
+  gameDragonHead,
+  gameExitDoor,
+  gameGears,
+  gameHelmet,
+  gameMagicPotion,
+  gameOpenTreasureChest,
+  gamePlayerBase,
+  gameScrollQuill,
+  gameScrollUnfurled,
+  gameSwordBrandish,
+  gameWizardStaff,
+} from '@ng-icons/game-icons';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [RouterLink, NgIcon, CurrencyPipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
+  providers: [
+    provideIcons({
+      gameBackpack,
+      gameHelmet,
+      gameWizardStaff,
+      gameBookPile,
+      gameMagicPotion,
+      gameDragonHead,
+      gameScrollUnfurled,
+      gameScrollQuill,
+      gameArchiveResearch,
+      gameGears,
+      gamePlayerBase,
+      gameOpenTreasureChest,
+      gameCrown,
+      gameExitDoor,
+      gameBoxUnpacking,
+      gameSwordBrandish,
+      gameBlackKnightHelm,
+    }),
+  ],
 })
 export class Navbar {
   cartCount = 0;
   cartTotal = 0;
-  userName = 'Knight'; // Would normally come from auth service
+  userName = 'Knight';
 
   constructor() {
     cartService.cart$.subscribe((items) => {
